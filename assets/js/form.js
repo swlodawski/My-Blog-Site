c12f0afbb3fd537f625c5f271eacf6938d17b370
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-10 12:40:01 -0400

    Third round of adding comments to logic files as a type of additional documentation.

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,7 +5,7 @@ const submit = document.getElementById('submitbutton');
 // I set variables for all my form elements from the index.html sheet that the user will intereact with and ffor which I want to pull the value inputs.
 
 function saveBlog() {
-    
+//I am declaring the variable for the function that will save the user input values that will be put into localstorage that will then be retreived and be rendered on the blog contebnt page. 
   const blogList = JSON.parse(localStorage.getItem('blogList'));
 
     const post = { 
@@ -16,34 +16,36 @@ function saveBlog() {
 // I am declaring an object named post with multiple key value pairs that I will push into an empty array of objects that will appear on the blog content page.
     if(blogList === null){
         localStorage.setItem('blogList', JSON.stringify([post]));
+//I am tellingt the computer to ensure that if the input values are null insure the user has inputted something in a fields. 
     } else {
         blogList.push(post)
-
+//If the user has inoputed all values then push that input into the object post. 
         localStorage.setItem('blogList', JSON.stringify(blogList));
+//I am telling the computer after the user input has been pushed into the object array to then store it in localstorage to later be retireved. As well as convert it to a JSON string value. 
     }
 }
 
     document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('myForm').addEventListener
-
+// I am setting an event listener for the submit form button on index.html sheet to register the user click interaction/.
 
     document.getElementById('myForm').addEventListener
     ('submit', function(event) {
         event.preventDefault();
-
+//I am setting a preventdefult event to keep the information from being cleared when the document is refreshed. 
     if(validateForm()) {
             saveBlog();
             window.location.href = 'blog.html';
+//I am setting a failsafe to validate the form user input before it is officially stored within the blog.html and then referencing what html page the input will render to. 
         }
     });
     });
-
 
 function validateForm() {
     const userName = document.getElementById('userbox').value.trim();
     const title = document.getElementById('titlebox').value.trim();
     const content = document.getElementById('contentbox').value.trim(); 
-    
+//I am catching the user input values for each inpurt box and then using the trim to remove any extra whitspace. 
     if( userName === '') {
         alert('Please Enter Your username');
         return false;
@@ -60,4 +62,4 @@ function validateForm() {
     }
     return true;
 }
-
+//I am creating a loop of alerts for each input area to ensure the user inputs a value into each box. Otherwise they will recieve an alert to fill the box. Once inputs are recieved the cycle will end and the code can continue to execute. 
